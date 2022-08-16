be3206ce5cf80acc08e363b38ee8ae8daaf48b18
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-16 10:46:39 +0300

    add contact implemented

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,7 +1,15 @@
-// const {contactsModel} = require('../../models')
+const { contactsModel } = require("../../models");
 
 const addContact = async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    res.status(400).json({ message: "missing required name field" });
+    return;
+  }
+
+  const result = await contactsModel.addContact(req.body);
+  res.status(201).json({ status: "success", code: 201, data: { result } });
 };
 
 module.exports = addContact;
