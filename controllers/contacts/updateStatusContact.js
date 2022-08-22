cd0f58b7dbd0b00b5239ac2c738c708e60fde87f
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-22 22:34:38 +0300

    bug fixed

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -1,12 +1,14 @@
 const { Contact } = require('../../models')
 
-const { RequestError } = require('../../helpers')
+const { requestError } = require('../../helpers')
 
 const updateStatusContact = async (req, res) => {
-  const { id } = req.params
-  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
+  const { contactId } = req.params
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  })
   if (!result) {
-    throw RequestError(404, 'Not found')
+    throw requestError(404, 'Not found')
   }
   res.status(200).json(result)
 }
