e2031a254bffea94b9f6f1827ae27288027556cb
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-30 23:11:06 +0300

    contacts api refined according to user account

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -4,9 +4,17 @@ const { requestError } = require('../../helpers')
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
-    new: true,
-  })
+  const { _id: owner } = req.user
+  console.log(req.user)
+  const result = await Contact.findOneAndUpdate(
+    { owner, _id: contactId },
+    req.body,
+    {
+      new: true,
+    }
+  )
+  console.log(result)
+
   if (!result) {
     throw requestError(404, 'Not found')
   }
