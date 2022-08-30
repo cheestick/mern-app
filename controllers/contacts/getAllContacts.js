520cfaa75f1c22a7993ea325a5e4d0e28c6be22e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-08-30 20:37:39 +0300

    pagination added

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,7 +1,13 @@
 const { Contact } = require('../../models')
 
-const getAllContacts = async (_, res, __) => {
-  const result = await Contact.find({}, '-createdAt -updatedAt')
+const getAllContacts = async (req, res, __) => {
+  const { _id: owner } = req.user
+  const { page = 1, limit = 5 } = req.query
+  const skip = (page - 1) * limit
+  const result = await Contact.find({ owner }, '-createdAt -updatedAt', {
+    skip,
+    limit: Number(limit),
+  })
   res.status(200).json(result)
 }
 
